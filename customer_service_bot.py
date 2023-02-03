04e61be9e4a27e239c2a97235890ccfe1611f55f
Soham Mehra <mehrasoham.mail@gmail.com>
2023-02-03 09:48:04 -0500

    Imported necessary modules for building bot

---
+++ customer_service_bot.py
@@ -0,0 +1,9 @@
+import json
+import numpy as np
+from tensorflow import keras
+from sklearn.preprocessing import LabelEncoder
+
+
+import colorama
+colorama.init()
+from colorama import Fore, Style, Back
